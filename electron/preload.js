690176ea0a67253b24cfe81b1e59df03727e9e71
Excellence D. <eoyeniran@tradedepot.co>
2025-08-07 10:16:14 +0100

    fix: remove electron

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-// electron/preload.js
-window.addEventListener("DOMContentLoaded", () => {
-  // You can safely expose APIs or run code here
-  // Example: Expose a version number
-  // window.versions = { node: process.versions.node };
-});
